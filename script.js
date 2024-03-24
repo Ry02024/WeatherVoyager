ffadac3d2378a5c0d5e2ddeb9b72a3ec0e28ee2b
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-03-25 06:52:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 // ここに上記のどちらかのJavaScriptコードを挿入
 document.body.innerHTML += '<p>こんにちは、世界！</p>';
-// または
-var newElement = document.createElement('p');
-newElement.textContent = 'こんにちは、世界！';
-document.body.appendChild(newElement);
