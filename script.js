cc001bc71df1ab8b02c0ecc4b4b4a719bbdef5b5
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-03-25 06:43:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var map = L.map('map').setView([35.681236, 139.767125], 13); // 東京駅を中心に地図を表示
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+<script>
+    alert("こんにちは、JavaScript！");
+</script>
